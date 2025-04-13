682dd9271775605f31b6a0bcf11df6b2fc484ede
unknown <19405@DESKTOP-3O636F2.buptnt.com>
2025-04-13 22:53:46 +0800

    commit src file

---
+++ src/java1/studenttest.java
@@ -0,0 +1,14 @@
+package java1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class studenttest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
